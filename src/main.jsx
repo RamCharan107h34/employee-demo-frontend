d9b4bb3f1319f44f7d4a2037e0a4d507bea0517a
RamCharan107h34 <24eg107h34@anurag.edu.in>
2026-05-18 14:27:52 +0530

    latest commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import ContextProvider from './components/ContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <StrictMode>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </StrictMode>
 )
